6231e8cad278b2d896ff1986cfeeb1a9016c4abf
Dmitry Teslya <dmitry.teslya@gmail.com>
2021-01-11 14:53:41 +0300

    basic web interface

---
+++ client/src/App.js
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import {
+    Box,
+    Button,
+    Text,
+    Collapsible,
+    Heading,
+    Grommet,
+    Layer,
+    Footer,
+    Anchor,
+    ResponsiveContext,
+  } from 'grommet';
+import { CircleQuestion, FormClose, Github } from 'grommet-icons';
+import { PidInput } from './components/pidinput/pidinput.component';
+import { Help } from './components/help/help.component';
+const theme = {
+  global: {
+    colors: {
+      brand: '#01A460',
+    },
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px',
+    },
+  },
+};
+const AppBar = (props) => (
+  <Box
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background='brand'
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation='medium'
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+);
+function App() {
+  const [showSidebar, setShowSidebar] = useState(false);
+  return (
+    <Grommet theme={theme} themeMode="dark" full>
+      <ResponsiveContext.Consumer>
+        {size => (
+      <Box fill>
+        <AppBar>
+          <Heading level='3' margin='none'>Cisco Support API Client</Heading>
+          <Button
+            icon={<CircleQuestion />}
+            onClick={() => setShowSidebar(!showSidebar)}
+          />        
+        </AppBar>
+        <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+          <Box flex align='center' justify='center'>
+              <PidInput />
+          </Box>
+          {(!showSidebar || size !== 'small') ? (
+            <Collapsible direction="horizontal" open={showSidebar}>
+              <Box
+                flex
+                width='medium'
+                background='light-2'
+                elevation='small'
+                align='start'
+                justify='center'
+              >
+                <Help />
+              </Box>
+            </Collapsible>
+          ): (
+            <Layer>
+              <Box
+                background='light-2'
+                tag='header'
+                justify='end'
+                align='center'
+                direction='row'
+              >
+                <Button
+                  icon={<FormClose />}
+                  onClick={() => setShowSidebar(false)}
+                />
+              </Box>
+              <Box
+                fill
+                background='light-2'
+                align='start'
+                justify='center'
+              >
+                <Help />
+              </Box>
+            </Layer>
+          )}
+        </Box>
+        <Footer justify="center" background="brand" pad="small">
+          <Anchor href="https://github.com/dteslya/cisco-api" target="_blank"><Github /></Anchor>
+        </Footer>
+      </Box>
+      )}
+      </ResponsiveContext.Consumer>
+    </Grommet>
+  );
+}
+
+export default App;
---
+++ main.py
@@ -0,0 +1,19 @@
+from oauthlib.oauth2 import BackendApplicationClient
+from requests_oauthlib import OAuth2Session
+from requests.auth import HTTPBasicAuth
+from pprint import pprint
+
+client_id = "fzgb3yyr6kvv5nfv8d2rzz5g"
+client_secret = "J4ESuMDjx2RarAqQK59qQbYR"
+
+auth = HTTPBasicAuth(client_id, client_secret)
+client = BackendApplicationClient(client_id=client_id)
+oauth = OAuth2Session(client=client)
+token = oauth.fetch_token(
+    token_url="https://cloudsso.cisco.com/as/token.oauth2", auth=auth
+)
+
+r = oauth.get(
+    "https://api.cisco.com/supporttools/eox/rest/5/EOXByProductID/1/ASR1002,WS-C3550-12G"
+)
+pprint(r.json())
