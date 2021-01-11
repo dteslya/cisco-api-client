d80a22159fff546f8a29ed1457b5d6e5c1420bcc
Dmitry Teslya <dmitry.teslya@gmail.com>
2021-01-11 14:53:41 +0300

    basic web interface

---
+++ client/src/components/help/help.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+    Box,
+    Markdown,
+  } from 'grommet';
+
+export const Help = () => {
+  return (
+    <Box pad="small">
+      <Markdown>{
+`## How to use
+
+1. Enter Cisco device PIDs separated by commas
+2. Press "Submit"
+`}
+      </Markdown>
+    </Box>
+  )
+}
---
+++ client/src/components/pidinput/pidinput.component.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { Box, Form, FormField, Button, TextArea} from 'grommet';
+
+export const PidInput = () => {
+
+  const [value, setValue] = React.useState({});
+
+  return (
+    <Form
+      value={value}
+      onChange={nextValue => setValue(nextValue)}
+      onReset={() => setValue({pids: ""})}
+      //onReset={() => console.log(value)}
+      onSubmit={() => console.log(value)}
+    >
+      <FormField
+        name="pids"
+        htmlfor="text-input-id"
+        label="Enter Product IDs"
+        htmlFor="text-area"
+        component={TextArea}
+        placeholder="Enter PID(s)"
+      />
+      <Box direction="row" gap="medium">
+        <Button type="submit" primary label="Submit" />
+        <Button type="reset" label="Reset" />
+      </Box>
+    </Form>
+  );
+}
+
+
