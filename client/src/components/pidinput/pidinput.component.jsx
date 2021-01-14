94aa8071a0a6f54c592c69bf69f24b4187947c26
Dmitry Teslya <dmitry.teslya@gmail.com>
2021-01-14 15:49:55 +0300

    changed input form size to medium

--- client/src/components/pidinput/pidinput.component.jsx
+++ client/src/components/pidinput/pidinput.component.jsx
@@ -13,50 +13,52 @@ export const PidInput = () => {
   // Looks up .env.local file when in development environment
   const backend_url = process.env.REACT_APP_BACKEND_URL;
   return (
-    <Form
-      validate="blur"
-      value={value}
-      onChange={nextValue => setValue(nextValue)}
-      onReset={() => setValue({ pids: "" })}
-      onSubmit={() => {
-        setEoxdata([])
-        trackPromise(
-          fetch(`${backend_url}/eox/`, {
-            method: 'POST',
-            headers: {
-              'Content-type': 'application/json',
+    <Box width="medium">
+      <Form
+        validate="blur"
+        value={value}
+        onChange={nextValue => setValue(nextValue)}
+        onReset={() => setValue({ pids: "" })}
+        onSubmit={() => {
+          setEoxdata([])
+          trackPromise(
+            fetch(`${backend_url}/eox/`, {
+              method: 'POST',
+              headers: {
+                'Content-type': 'application/json',
+              },
+              // remove spaces from input
+              body: JSON.stringify(value).replace(/\s/g, '')
+              //body: JSON.stringify(value).replace(/(\r\n|\n|\r)/gim, ",")
+            })
+              .then(res => res.json())
+              .then(setEoxdata)
+          )
+        }
+        }
+      >
+        <FormField
+          name="pids"
+          htmlfor="text-input-id"
+          label="Enter Product IDs"
+          htmlFor="text-area"
+          component={TextArea}
+          placeholder="WS-C6506-E,CISCO2901/K9"
+          required
+          validate={[
+            { regexp: /^[a-z0-9]*[a-z0-9/-]*\s*,*\s*[a-z0-9]+[a-z0-9/-]*\s*/i },
+            name => {
+              if (name && name.length === 1) return 'must be >1 character';
+              return undefined;
             },
-            // remove spaces from input
-            body: JSON.stringify(value).replace(/\s/g, '')
-            //body: JSON.stringify(value).replace(/(\r\n|\n|\r)/gim, ",")
-          })
-            .then(res => res.json())
-            .then(setEoxdata)
-        )
-      }
-      }
-    >
-      <FormField
-        name="pids"
-        htmlfor="text-input-id"
-        label="Enter Product IDs"
-        htmlFor="text-area"
-        component={TextArea}
-        placeholder="WS-C6506-E,CISCO2901/K9"
-        required
-        validate={[
-          { regexp: /^[a-z0-9]*[a-z0-9/-]*\s*,*\s*[a-z0-9]+[a-z0-9/-]*\s*/i },
-          name => {
-            if (name && name.length === 1) return 'must be >1 character';
-            return undefined;
-          },
-        ]}
-      />
-      <Box direction="row" gap="medium">
-        <Button type="submit" primary label="Submit" />
-        <Button type="reset" label="Reset" />
-      </Box>
-    </Form>
+          ]}
+        />
+        <Box direction="row" justify="between" margin={{ top: 'medium' }}>
+          <Button type="submit" primary label="Submit" />
+          <Button type="reset" label="Reset" />
+        </Box>
+      </Form>
+    </Box>
   );
 }
 
