2751a30accb30253097d55ad8856991fc38c3cd2
Dmitry Teslya <dmitry.teslya@gmail.com>
2021-01-15 13:58:57 +0300

    infinite scroll

--- client/src/components/list/list.component.jsx
+++ client/src/components/list/list.component.jsx
@@ -7,24 +7,36 @@ import { AppContext } from '../../App';
 
 export const ListEOL = () => {
     const eoxdata = useContext(AppContext);
+    // items to display
+    const step = 10
 
+    // define datatable columns
     const columns = [
+        {
+            property: 'key',
+            header: 'Num',
+            primary: true,
+            size: "xxsmall"
+        },
         {
             property: 'EOLProductID',
             header: "Device Product Number",
-            primary: true,
+            size: "medium"
         },
         {
             property: 'EOXExternalAnnouncementDate',
             header: 'End Of Life',
+            size: "xsmall"
         },
         {
             property: 'EndOfSaleDate',
-            header: 'End Of Sale'
+            header: 'End Of Sale',
+            size: "xsmall"
         },
         {
             property: 'LastDateOfSupport',
-            header: 'End Of Support'
+            header: 'End Of Support',
+            size: "xsmall"
         },
         {
             property: 'MigrationProductName',
@@ -32,10 +44,20 @@ export const ListEOL = () => {
         }
 
     ]
-    //console.log("EOX list:", eoxdata);
     return (
-        <Box align="center" pad="large">
-            <DataTable columns={columns} data={eoxdata.eoxdata} step={10} />
+        <Box align="center" fill="horizontal" pad="medium" flex>
+            <DataTable
+                //columns={columns}
+                columns={columns.map(c => ({
+                    ...c,
+                    search: c.property === 'EOLProductID',
+                }))}
+                data={eoxdata.eoxdata}
+                step={step}
+                size="medium"
+                //resizeable
+                sortable
+                onMore={() => { console.log(`InfiniteScroll fires onMore after loading ${step} items`) }} />
         </Box>
     );
 }
